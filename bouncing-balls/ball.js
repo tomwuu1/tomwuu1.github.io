07ee496c7b78e0839cd6dcff4cdab6d61b93487d
tomwuu <75061878+tomwuu1@users.noreply.github.com>
2021-11-30 10:46:40 +0800

    modified:   CSS/1.css 	new file:   CSS/login.css 	new file:   HTML/login.html 	new file:   HTML/register.html 	new file:   JavaScript/login.js 	modified:   JavaScript/main.js 	new file:   JavaScript/register.js 	new file:   bouncing-balls/ball.html 	new file:   bouncing-balls/ball.js 	new file:   bouncing-balls/style.css 	new file:   image/favicon (1).ico 	new file:   image/guanniaowang/dove.png 	new file:   image/guanniaowang/favorite-1.jpg 	new file:   image/guanniaowang/favorite-1_th.jpg 	new file:   image/guanniaowang/favorite-2.jpg 	new file:   image/guanniaowang/favorite-2_th.jpg 	new file:   image/guanniaowang/favorite-3.jpg 	new file:   image/guanniaowang/favorite-3_th.jpg 	new file:   image/guanniaowang/favorite-4.jpg 	new file:   image/guanniaowang/favorite-4_th.jpg 	modified:   index.html 	new file:   php/1.php

---
+++ bouncing-balls/ball.js
@@ -0,0 +1,194 @@
+// 设置画布
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const p = document.querySelector('p');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+let count;
+// 生成随机数的函数
+
+function random(min,max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
+
+// 生成随机颜色值的函数
+
+function randomColor() {
+  const color = 'rgb(' +
+                random(0, 255) + ',' +
+                random(0, 255) + ',' +
+                random(0, 255) + ')';
+  return color;
+}
+
+// 定义 Ball 构造器
+
+function Shape(x, y, velX, velY,exists) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.exists = exists;
+}
+
+function Ball(x ,y , velX, velY, color, size,exists) {
+  Shape.call(this,x,y,velX,velY,exists);
+  this.color = color;
+  this.size = size;
+}
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
+// 定义彩球绘制函数
+function EvilCircle(x,y,exists) {
+  Shape.call(this,x,y,20,20,exists);
+  this.color =randomColor();
+  this.size = 10;
+}
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+// 定义彩球更新函数
+
+Ball.prototype.update = function() {
+  if((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+// 定义碰撞检测函数
+
+Ball.prototype.collisionDetect = function() {
+  for(let j = 0; j < balls.length; j++) {
+    if(this !== balls[j]) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = randomColor();
+      }
+    }
+  }
+};
+
+// 定义一个数组，生成并保存所有的球
+EvilCircle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.strokeStyle = this.color;
+  ctx.lineWidth = 3;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+EvilCircle.prototype.checkBounds = function() {
+  if((this.x + this.size) >= width) {
+    this.x -= this.size;
+  }
+
+  if((this.x - this.size) <= 0) {
+    this.x += this.size;
+  }
+
+  if((this.y + this.size) >= height) {
+    this.y -= this.size;
+  }
+
+  if((this.y - this.size) <= 0) {
+    this.y += this.size;
+  }
+
+}
+EvilCircle.prototype.setControls = function() {
+  window.onkeydown = e => {
+    switch(e.key) {
+      case 'a':
+        this.x -= this.velX;
+        break;
+      case 'd':
+        this.x += this.velX;
+        break;
+      case 'w':
+        this.y -= this.velY;
+        break;
+      case 's':
+        this.y += this.velY;
+        break;
+    }
+  };
+}
+EvilCircle.prototype.collisionDetect = function() {
+  for(let j = 0; j < balls.length; j++) {
+    if(balls[j].exists) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+        count--;
+        p.textContent = '还剩' + count + '个';
+      }
+    }
+  }
+}
+let balls = [];
+
+while(balls.length < 25) {
+  const size = random(10,20);
+  let ball = new Ball(
+    // 为避免绘制错误，球至少离画布边缘球本身一倍宽度的距离
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomColor(),
+    size,
+    true
+  );
+  balls.push(ball);
+}
+count = balls.length;
+p.textContent = '还剩' + count + '个';
+// 定义一个循环来不停地播放
+let circle = new EvilCircle(random(0, width), random(0, height), true);
+circle.setControls();
+function loop() {
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0,0,width,height);
+  for(let i = 0; i < balls.length; i++) {
+    if(balls[i].exists){
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+    circle.draw();
+    circle.checkBounds();
+    circle.collisionDetect();
+  }
+  requestAnimationFrame(loop);
+}
+
+loop();
